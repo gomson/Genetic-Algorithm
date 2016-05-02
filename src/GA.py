ec168ee2ca3ded8e788df0fdece93a91244d4159
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-05-02 16:28:07 -0500

    Update GA.py

--- src/GA.py
+++ src/GA.py
@@ -1,7 +1,10 @@
+# @Auther="Yuanhui Yang"
+# @Email="yuanhui.yang@u.northwestern.edu"
+
 from math import log, ceil, pow, exp, fabs
 from random import random, randint, randrange
 
-def fitnessfun(x):#输入popsize x UnitLength行向量
+def fitnessfun(x):
                 global popsize
                 Fitvalue = [0 for col in range(popsize)]
                 for i in range(popsize):
@@ -14,9 +17,9 @@ def fitnessfun(x):#输入popsize x UnitLength行向量
                 for i in range(popsize):
                                 P_Cumsum[i] /= Fitvalue_Cumsum[-1]
                 y = [Fitvalue, P_Cumsum]
-                return y#输出2 x popsize矩阵
+                return y
 
-def transform(x):#输入1 x UnitLength行向量, 即1条染色体
+def transform(x):
                 global BitLength
                 global boundsbegin
                 global delta
@@ -27,21 +30,20 @@ def transform(x):#输入1 x UnitLength行向量, 即1条染色体
                                                 y[i] += x[i * BitLength[i] + j] * pow(2, BitLength[i] - 1 - j)
                                 y[i] *= delta[i]
                                 y[i] += boundsbegin[i]
-                return  y#输出1 x VarNum行向量, 即染色体对应自变量
+                return  y
 
-def targetfun(x):#输入1 x VarNum行向量
-                #边界处理
+def targetfun(x):
                 global Initial
                 g1 = 1.5 + x[0] * x[1] - x[0] - x[1]
                 g2 = -x[0] * x[1]
                 if g1 > 0 or g2 >10:
                                 y = 0
                 else:
-                                y = Initial + exp(x[0]) * (4 * x[0] ** 2 + 2 * x[1] ** 2 + 4 * x[0] * x[1] + 2 * x[1] + 1)#\[\exp \left( {x\left[ 0 \right]} \right) \times \left( {4 \times x{{\left[ 0 \right]}^2} + 2 \times x{{\left[ 1 \right]}^2} + 4 \times x\left[ 0 \right] \times x\left[ 1 \right] + 2 \times x\left[ 1 \right] + 1} \right)\]
+                                y = Initial + exp(x[0]) * (4 * x[0] ** 2 + 2 * x[1] ** 2 + 4 * x[0] * x[1] + 2 * x[1] + 1)
                                 y = 1 / y
-                return y#输出1个实数, 即染色体对应适应度值
+                return y
 
-def selection(x):#输入1 x UnitLength行向量
+def selection(x):
                 y = [0 for col in range(2)]
                 for i in range(2):
                                 temp = random()
@@ -49,9 +51,9 @@ def selection(x):#输入1 x UnitLength行向量
                                 while x[j] < temp:
                                                 j += 1
                                 y[i] = j
-                return y#输出1x 2行向量
+                return y
 
-def mutation(x):#输入1 x UnitLength行向量
+def mutation(x):
                 global pmutation
                 global UnitLength
                 y = x[:]
@@ -60,9 +62,9 @@ def mutation(x):#输入1 x UnitLength行向量
                                 temp2 = randrange(UnitLength)
                                 temp2 = int(temp2)
                                 y[temp2] = fabs(x[temp2] - 1)
-                return y#输出1xUnitLength行向量
+                return y
 
-def IfCroIfMut(x):#输入1 x 1纯小数
+def IfCroIfMut(x):
                 temp1 = [0 for col in range(10000)]
                 temp2 = 10000 * x
                 temp2 = ceil(temp2)
@@ -70,9 +72,9 @@ def IfCroIfMut(x):#输入1 x 1纯小数
                 temp1[0: temp2] = [1 for col in range(temp2)]
                 temp3 = randrange(10000)
                 y = temp1[temp3]
-                return y#输出1 x 1, 0或1
+                return y
 
-def crossover(x):#输入2 x UnitLength矩阵
+def crossover(x):
                 global UnitLength
                 global pcrossover
                 y = [[0 for col in range(UnitLength)] for row in range(2)]
@@ -84,30 +86,30 @@ def crossover(x):#输入2 x UnitLength矩阵
                 else:
                                 y[0] = x[0]
                                 y[1] = x[1]
-                return y#输出2 x UnitLength矩阵
+                return y
 
 def main():
-                global BitLength#染色体长度(1维数组)
-                global UnitLength#1条染色体长度
-                global boundsbegin#变量下边界(矩形)
-                global boundsend#变量上边界(矩形)
-                global popsize#种群规模
-                global pcrossover#交配概率
-                global pmutation#变异概率
-                global delta#编码精度
-                global Initial#确保适应度为正值
-                global VarNum#变量数
-                Initial = 100#确保适应度为正值
-                pcrossover = 0.1#交配概率
-                pmutation = 0.4#变异概率
-                UnitLength = 0#1条染色体长度
-                boundsbegin = [-10, -10]#变量下边界(矩形)
-                boundsend = [10, 10]#变量上边界(矩形)
-                VarNum = len(boundsbegin)#变量数
-                BitLength = [0 for col in range(VarNum)]#染色体长度(1维数组)
-                delta = [0 for col in range(VarNum)]#编码精度
+                global BitLength
+                global UnitLength
+                global boundsbegin
+                global boundsend
+                global popsize
+                global pcrossover
+                global pmutation
+                global delta
+                global Initial
+                global VarNum
+                Initial = 100
+                pcrossover = 0.1
+                pmutation = 0.4
+                UnitLength = 0
+                boundsbegin = [-10, -10]
+                boundsend = [10, 10]
+                VarNum = len(boundsbegin)
+                BitLength = [0 for col in range(VarNum)]
+                delta = [0 for col in range(VarNum)]
                 precision = [1e-4, 1e-4]
-                popsize = 100#种群规模
+                popsize = 100
                 temp = popsize % 2
                 if temp ==0:
                                 popsize =popsize
